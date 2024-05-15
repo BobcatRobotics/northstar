983a55090082a0a5b28e3896bccbad5f00696382
Lunar-Academy <69018644+Lunar-Academy@users.noreply.github.com>
2024-05-15 18:24:13 +0800

    Update Capture.py

--- vision/northstar/pipeline/Capture.py
+++ vision/northstar/pipeline/Capture.py
@@ -83,7 +83,8 @@ class GStreamerCapture(Capture):
                 print("Starting capture session")
 #                self._video = cv2.VideoCapture("v4l2src device=" + str(config_store.remote_config.camera_id) + " extra_controls=\"c,exposure_auto=" + str(config_store.remote_config.camera_auto_exposure) + ",exposure_absolute=" + str(
 #                   config_store.remote_config.camera_exposure) + ",gain=" + str(config_store.remote_config.camera_gain) + ",sharpness=0,brightness=0\" ! image/jpeg,format=MJPG,width=" + str(config_store.remote_config.camera_resolution_width) + ",height=" + str(config_store.remote_config.camera_resolution_height) + " ! jpegdec ! video/x-raw ! appsink drop=1", cv2.CAP_GSTREAMER)
-                self._video = cv2.VideoCapture(int(config_store.remote_config.camera_id),cv2.CAP_V4L2)
+                # self._video = cv2.VideoCapture(int(config_store.remote_config.camera_id),cv2.CAP_V4L2)
+                self._video = cv2.VideoCapture("/dev/"+str(config_store.remote_config.camera_id),cv2.CAP_V4L2)
                 self._video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
                 self._video.set(cv2.CAP_PROP_FPS, config_store.remote_config.frame_rate)
                 # self._video.set(cv2.CAP_PROP_FRAME_WIDTH, 1600)
